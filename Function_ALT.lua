e777f3c9aa456bdda05ccdd71758d7bc3a9d5d75
Fatex <85951383+StwFate@users.noreply.github.com>
2022-06-12 19:20:18 -0400

    Update Function_ALT.lua

--- Function_ALT.lua
+++ Function_ALT.lua
@@ -2,16 +2,15 @@ coroutine.wrap(function()
     syn.queue_on_teleport([[
         repeat task.wait() until game:IsLoaded()
             if game.PlaceId == 5166670285 then
-            repeat task.wait() until workspace:FindFirstChild("100")
-
+            wait(2) -- wait for the shit to load lmao
             for _,v in next, workspace:GetChildren() do
-                if tonumber(v.Name) then
+                if tonumber(v.Name) and tonumber(v.Name) < 500 then
                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Platform, 0)
                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Platform, 1)  
                     wait()
                 end
             end
-            wait(8)
+            wait(5) -- registering the shit happen
             game.Players.LocalPlayer:Kick("done")
         end]])
 end)()
